653f276631f35eed22b03c67240e1108d965dbb2
uzair142 <113887346+uzair142@users.noreply.github.com>
2023-04-14 17:55:18 +0100

    padding top

--- src/App.js
+++ src/App.js
@@ -28,7 +28,36 @@ function App() {
   }
 
   function anagramCompiler(words) {
-    // ... unchanged from before ...
+    const anagramMap = new Map();
+
+    // Loop through the words array
+    for (let i = 0; i < words.length; i++) {
+      // Remove non-letter characters, convert to lowercase, sort, and join the letters to create a sorted word
+      const sorted = words[i]
+        .replace(/[^a-z]/gi, "")
+        .toLowerCase()
+        .split("")
+        .sort()
+        .join("");
+      // If the sorted word is already in the Map, add the original word to its array of anagrams
+      if (anagramMap.has(sorted)) {
+        anagramMap.get(sorted).push(words[i]);
+      }
+      // else, key-value pair with the sorted word as key and an array containing the original word as value
+      else {
+        anagramMap.set(sorted, [words[i]]);
+      }
+    }
+
+    // Initialize an empty array to store the result
+    let result = [];
+    // Loop through the entries of the anagramMap
+    for (const [sortedWord, anagrams] of anagramMap) {
+      // Join the array of anagrams with a space separator and add it to the result array
+      result.push(anagrams.join(" "));
+    }
+
+    return result;
   }
 
   function onClick() {
