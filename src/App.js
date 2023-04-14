b49a664967d070344400a4b1eec2bce923cea83d
uzair142 <113887346+uzair142@users.noreply.github.com>
2023-04-14 17:49:45 +0100

    Anagram information and top padding

--- src/App.js
+++ src/App.js
@@ -36,16 +36,16 @@ function App() {
   }
 
   return (
-    <div class="bg-gradient-to-br from-purple-600 via-pink-500 to-red-500 text-white font-bold text-2xl text-center min-h-screen">
+    <div className="bg-gradient-to-br from-purple-600 via-pink-500 to-red-500 text-white font-bold text-2xl text-center min-h-screen pt-5">
       {showModal && (
-        <div class="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
-          <div class="bg-white rounded-lg p-8 max-w-sm mx-auto">
-            <h2 class="text-xl font-semibold mb-4">Invalid file type</h2>
-            <p class="text-gray-700">
+        <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
+          <div className="bg-white rounded-lg p-8 max-w-sm mx-auto">
+            <h2 className="text-xl font-semibold mb-4">Invalid file type</h2>
+            <p className="text-gray-700">
               The selected file is not a text file. Please select a .txt file.
             </p>
             <button
-              class="mt-6 px-4 py-2 bg-red-500 text-white rounded-lg font-semibold"
+              className="mt-6 px-4 py-2 bg-red-500 text-white rounded-lg font-semibold"
               onClick={() => setShowModal(false)}
             >
               OK
@@ -53,22 +53,23 @@ function App() {
           </div>
         </div>
       )}
-      <div class="mx-auto max-w-3xl p-8 rounded-lg shadow-lg bg-white">
-        <h1 class="mb-8 text-4xl font-extrabold text-gray-800">
-          How many Anagrams can you spot?
-        </h1>
-        <div class="bg-gray-100 rounded-lg shadow-md p-8">
+      <div className="mx-auto max-w-3xl p-8 rounded-lg shadow-lg bg-white">
+        <div className="bg-gray-100 rounded-lg shadow-md p-8">
+          <div className="bg-green-100 border-l-4 border-green-500 text-green-700 p-4 mb-8" role="alert">
+            <p className="font-bold">What is an anagram?</p>
+            <p>An anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.</p>
+          </div>
           <input
             type="file"
             name="file"
-            class="mb-8 py-2 px-4 border border-gray-400 rounded-lg shadow-sm w-full"
+            className="mb-8 py-2 px-4 border border-gray-400 rounded-lg shadow-sm w-full"
             onClick={onClick}
             onChange={(e) => onChange(e)}
           />
-          <div class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
             {anagrams.map((anagram, index) => (
               <div key={index} className="bg-white rounded-lg p-6 shadow-md">
-                <p class="text-pink-500 font-medium text-lg">{anagram}</p>
+                <p className="text-pink-500 font-medium text-lg">{anagram}</p>
               </div>
             ))}
           </div>
