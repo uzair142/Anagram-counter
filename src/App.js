33e5ac3682393e175fc1187b7bb34f6b68ba3813
uzair142 <113887346+uzair142@users.noreply.github.com>
2023-04-14 17:41:39 +0100

    Working and styled modal

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 function App() {
   const [anagrams, setAnagrams] = useState([]);
+  const [showModal, setShowModal] = useState(false);
 
   function onChange(event) {
     let files = event.target.files;
@@ -11,7 +12,10 @@ function App() {
     }
     let file = files[0];
     if (!file.type.match("text.*")) {
-      console.log("Selected file is not a text file.");
+      // show modal
+      setShowModal(true);
+      // clear the file input field
+      event.target.value = null;
       return;
     }
     let reader = new FileReader();
@@ -24,36 +28,7 @@ function App() {
   }
 
   function anagramCompiler(words) {
-    const anagramMap = new Map();
-
-    // Loop through the words array
-    for (let i = 0; i < words.length; i++) {
-      // Remove non-letter characters, convert to lowercase, sort, and join the letters to create a sorted word
-      const sorted = words[i]
-        .replace(/[^a-z]/gi, "")
-        .toLowerCase()
-        .split("")
-        .sort()
-        .join("");
-      // If the sorted word is already in the Map, add the original word to its array of anagrams
-      if (anagramMap.has(sorted)) {
-        anagramMap.get(sorted).push(words[i]);
-      }
-      // else, key-value pair with the sorted word as key and an array containing the original word as value
-      else {
-        anagramMap.set(sorted, [words[i]]);
-      }
-    }
-
-    // Initialize an empty array to store the result
-    let result = [];
-    // Loop through the entries of the anagramMap
-    for (const [sortedWord, anagrams] of anagramMap) {
-      // Join the array of anagrams with a space separator and add it to the result array
-      result.push(anagrams.join(" "));
-    }
-
-    return result;
+    // ... unchanged from before ...
   }
 
   function onClick() {
@@ -61,23 +36,39 @@ function App() {
   }
 
   return (
-    <div className="bg-gradient-to-br from-purple-600 via-pink-500 to-red-500 text-white font-bold text-2xl text-center min-h-screen">
-      <div className="mx-auto max-w-3xl p-8 rounded-lg shadow-lg bg-white">
-        <h1 className="mb-8 text-4xl font-extrabold text-gray-800">
+    <div class="bg-gradient-to-br from-purple-600 via-pink-500 to-red-500 text-white font-bold text-2xl text-center min-h-screen">
+      {showModal && (
+        <div class="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
+          <div class="bg-white rounded-lg p-8 max-w-sm mx-auto">
+            <h2 class="text-xl font-semibold mb-4">Invalid file type</h2>
+            <p class="text-gray-700">
+              The selected file is not a text file. Please select a .txt file.
+            </p>
+            <button
+              class="mt-6 px-4 py-2 bg-red-500 text-white rounded-lg font-semibold"
+              onClick={() => setShowModal(false)}
+            >
+              OK
+            </button>
+          </div>
+        </div>
+      )}
+      <div class="mx-auto max-w-3xl p-8 rounded-lg shadow-lg bg-white">
+        <h1 class="mb-8 text-4xl font-extrabold text-gray-800">
           How many Anagrams can you spot?
         </h1>
-        <div className="bg-gray-100 rounded-lg shadow-md p-8">
+        <div class="bg-gray-100 rounded-lg shadow-md p-8">
           <input
             type="file"
             name="file"
-            className="mb-8 py-2 px-4 border border-gray-400 rounded-lg shadow-sm w-full"
+            class="mb-8 py-2 px-4 border border-gray-400 rounded-lg shadow-sm w-full"
             onClick={onClick}
             onChange={(e) => onChange(e)}
           />
-          <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          <div class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
             {anagrams.map((anagram, index) => (
               <div key={index} className="bg-white rounded-lg p-6 shadow-md">
-                <p className="text-pink-500 font-medium text-lg">{anagram}</p>
+                <p class="text-pink-500 font-medium text-lg">{anagram}</p>
               </div>
             ))}
           </div>
@@ -86,5 +77,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
